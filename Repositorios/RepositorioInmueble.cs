2725a1e7d011c7e27aa148c569066fa29c1a7da5
Estebaan93 <94663767+Estebaan93@users.noreply.github.com>
2025-08-25 21:32:04 -0300

    cambios en el select

--- Repositorios/RepositorioInmueble.cs
+++ Repositorios/RepositorioInmueble.cs
@@ -21,9 +21,13 @@ namespace Inmobiliaria25.Repositorios
       using (var conn = _context.GetConnection())
       {
         conn.Open();
-        string sql = $@" SELECT {nameof(Inmuebles.idInmueble)}, {nameof(Inmuebles.idPropietario)}, {nameof(Inmuebles.idDireccion)}, {nameof(Inmuebles.idTipo)}, {nameof(Inmuebles.metros2)}, 
+        /*string sql = $@" SELECT {nameof(Inmuebles.idInmueble)}, {nameof(Inmuebles.idPropietario)}, {nameof(Inmuebles.idDireccion)}, {nameof(Inmuebles.idTipo)}, {nameof(Inmuebles.metros2)}, 
         {nameof(Inmuebles.cantidadAmbientes)}, {nameof(Inmuebles.disponible)}, {nameof(Inmuebles.precio)}, {nameof(Inmuebles.descripcion)}, {nameof(Inmuebles.cochera)}, {nameof(Inmuebles.piscina)}, 
-        {nameof(Inmuebles.mascotas)}, {nameof(Inmuebles.urlImagen)} FROM inmueble WHERE estado=1 ";
+        {nameof(Inmuebles.mascotas)}, {nameof(Inmuebles.urlImagen)} FROM inmueble WHERE estado=1 ";*/
+        string sql= @"SELECT idInmueble, idPropietario, idDireccion, idTipo, metros2, cantidadAmbientes, disponible, 
+                              precio, descripcion, cochera, piscina, mascotas, urlImagen, estado
+                        FROM inmueble
+                        WHERE estado = 1";
         using (var cmd = new MySqlCommand(sql, conn))
         using (var reader = cmd.ExecuteReader())
         {
@@ -31,15 +35,15 @@ namespace Inmobiliaria25.Repositorios
           {
             inmueble.Add(new Inmuebles
             {
-              idInmueble = reader.GetInt32(nameof(Inmuebles.idDireccion)),
-              idPropietario = reader.GetInt32(nameof(Inmuebles.idPropietario)),
-              idDireccion = reader.GetInt32(nameof(Inmuebles.idDireccion)),
-              idTipo = reader.GetInt32(nameof(Inmuebles.idTipo)),
-              metros2 = reader.GetString(nameof(Inmuebles.metros2)),
-              cantidadAmbientes = reader.GetInt32(nameof(Inmuebles.cantidadAmbientes)),
-              disponible= reader.GetBoolean(nameof(Inmuebles.disponible)),
+              idInmueble = reader.GetInt32("idInmueble"),
+              idPropietario = reader.GetInt32("idPropietario"),
+              idDireccion = reader.GetInt32("idDireccion"),
+              idTipo = reader.GetInt32("idTipo"),
+              metros2 = reader.GetString("metros2"),
+              cantidadAmbientes = reader.GetInt32("cantidadAmbientes"),
 
             });
+
           }
         }
 
